bdaec1d78b20d3ac1d41d04f9359dbc54ec0bb9e
Ariel Küchler <akuechl@users.noreply.github.com>
2021-02-07 22:50:27 +0100

    optimize

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,39 @@
 #![crate_name = "jpeg_diff_rs"]
 
 use image::io::Reader as ImageReader;
-use image::Rgb;
+use image::{DynamicImage, RgbImage};
 use std::io::Error;
-
-pub fn validate_files(value: String) -> Result<(), String> {
-    match value.parse::<usize>() {
-        Ok(_) => Ok(()),
-        _ => Err(format!(r#"Value have to be a number, not "{}"."#, &value)),
-    }
-}
+//use std::time::{SystemTime};
 
 pub fn run(files: Vec<&str>) -> Result<f32, Error> {
     let reference = files[0];
     let to_compare = &files[1..];
-    
+//   let now = SystemTime::now();
+
+    let image_reference = load_rgb8(reference)?;
     let mut max = 0f32;
     for file in to_compare {
-        let diff = load_image(reference, file)?;
+        let image2 = load_rgb8(file)?;
+        let diff = calculate_diff(&image_reference, &image2)?;
         let calculated_diff = diff.0 as f32 / diff.1 as f32;
         max = if max > calculated_diff { max } else { calculated_diff };
     }
+/*
+    match now.elapsed() {
+        Ok(elapsed) => {
+            println!("{:?}", elapsed);
+        }
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
+*/
     Ok(max)
 }
 
-pub fn load_image(file1: &str, file2: &str) -> Result<(u32, u32), Error>
-{
-    let img1 = ImageReader::open(file1)?.decode();
-    let image1 = match img1 {
-        Ok(i) => i.into_rgb8(),
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-    };
-    let img2 = ImageReader::open(file2)?.decode();
-    let image2 = match img2 {
-        Ok(i) => i.into_rgb8(),
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-    };
 
+fn calculate_diff(image1: &RgbImage, image2: &RgbImage) -> Result<(u32, u32), Error>
+{
     if image1.width() != image2.width() {
         panic!("Different widths")
     }
@@ -45,12 +42,19 @@ pub fn load_image(file1: &str, file2: &str) -> Result<(u32, u32), Error>
     }
 
     let mut diff = 0u32;
-    for x in 0..image1.width() {
-        for y in 0..image1.height() {
-            let lum1 = get_luminance_value(image1.get_pixel(x, y));
-            let lum2 = get_luminance_value(image2.get_pixel(x, y));
-            diff += (lum1 - lum2).abs() as u32;
-        }
+    
+    // using raw container is fast as get_pixel - I need speed
+    let container1 = image1.as_raw();
+    let container2 = image2.as_raw();
+    let mut i = 0;
+    while i < container1.len() {
+        let i3 = i + 3;
+        let rgb1 = &container1[i..i3];
+        let lum1 = get_luminance_value(rgb1);
+        let rgb2 = &container2[i..i3];
+        let lum2 = get_luminance_value(rgb2);
+        diff += (lum1 - lum2).abs() as u32;
+        i = i3;
     }
 
     let result = (diff, image1.width() * image1.height());
@@ -58,6 +62,25 @@ pub fn load_image(file1: &str, file2: &str) -> Result<(u32, u32), Error>
 }
 
 #[inline(always)]
-fn get_luminance_value(pix: &Rgb<u8>) -> i32 {
-    ((pix[0] as i32) * 299 + (pix[1] as i32) * 587 + (pix[2] as i32) * 114) / 1000
+fn load_rgb8(file: &str) -> Result<RgbImage, Error> {
+    let img = ImageReader::open(file)?.decode();
+    match img {
+        Ok(i) => {
+            match i {
+                DynamicImage::ImageRgb8(i) => Ok(i), // should be RGB8
+                _ => {
+                    //println!("No rgb8 {}. Need to convert.", file);
+                    Ok(i.to_rgb8())
+                }
+            }
+        }, 
+        Err(error) => panic!("Problem opening the file: {:?}", error)
+    }
+}
+
+#[inline(always)]
+fn get_luminance_value(pix: &[u8]) -> i32 {
+    // https://de.wikipedia.org/wiki/Luminanz
+    // Y' = 0,2126 R' + 0,7152 G' + 0,0722 B'(Rec. 709)
+    ((pix[0] as i32) * 2126 + (pix[1] as i32) * 7152 + (pix[2] as i32) * 722) / 10000
 }
